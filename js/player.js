ea5ca511a3a72626ecdda2624bfb36bfb4e06681
Shadow-Trigger <aoreilly30@belvederecollege.ie>
2026-01-08 09:17:51 +0000

    Update player.js

--- js/player.js
+++ js/player.js
@@ -1,1 +1,34 @@
+export class Player {
+  constructor(lanes, canvasHeight) {
+    this.lanes = lanes;
+    this.laneIndex = 2;
+    this.size = 40;
+    this.y = canvasHeight - 80;
+
+    window.addEventListener("keydown", e => {
+      if (e.key === "ArrowLeft" && this.laneIndex > 0) {
+        this.laneIndex--;
+      }
+      if (e.key === "ArrowRight" && this.laneIndex < lanes.count - 1) {
+        this.laneIndex++;
+      }
+    });
+  }
+
+  get x() {
+    return this.lanes.getLaneX(this.laneIndex);
+  }
+
+  update() {}
+
+  render(ctx) {
+    ctx.fillStyle = "green";
+    ctx.fillRect(
+      this.x - this.size / 2,
+      this.y - this.size / 2,
+      this.size,
+      this.size
+    );
+  }
+}
 
