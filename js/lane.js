3efc76c2e5b3da2d7fddca3d327788df59e8487b
Shadow-Trigger <aoreilly30@belvederecollege.ie>
2026-01-08 09:17:32 +0000

    Update lane.js

--- js/lane.js
+++ js/lane.js
@@ -1,1 +1,11 @@
+export class LaneManager {
+  constructor(count, width) {
+    this.count = count;
+    this.laneWidth = width / count;
+  }
+
+  getLaneX(index) {
+    return index * this.laneWidth + this.laneWidth / 2;
+  }
+}
 
