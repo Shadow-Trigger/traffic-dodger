b62b4986a409a76f9368a22d09e8ad5e3722b533
Shadow-Trigger <aoreilly30@belvederecollege.ie>
2026-01-09 16:59:55 +0000

    Update player.js

--- js/player.js
+++ js/player.js
@@ -2,17 +2,24 @@ export class Player {
   constructor(lanes, canvasHeight) {
     this.lanes = lanes;
     this.laneIndex = 2;
+    this.x = this.lanes.getLaneX(this.laneIndex);
+    this.targetX = this.x;
+    this.moveSpeed = 0.6; // tweak this for difficulty
     this.size = 40;
     this.y = canvasHeight - 80;
 
     window.addEventListener("keydown", e => {
-      if (e.key === "ArrowLeft" && this.laneIndex > 0) {
-        this.laneIndex--;
-      }
-      if (e.key === "ArrowRight" && this.laneIndex < lanes.count - 1) {
-        this.laneIndex++;
-      }
-    });
+  if (e.key === "ArrowLeft" && this.laneIndex > 0) {
+    this.laneIndex--;
+    this.targetX = this.lanes.getLaneX(this.laneIndex);
+  }
+
+  if (e.key === "ArrowRight" && this.laneIndex < this.lanes.count - 1) {
+    this.laneIndex++;
+    this.targetX = this.lanes.getLaneX(this.laneIndex);
+  }
+});
+    
   }
 
   get x() {
